bf514ee584c287a18057a945180a378ec8917c4a
David <perpar.david@gmail.com>
2023-11-02 22:24:35 +0100

    Superhero service tests

--- src/test/java/com/davidperezpardo/superheroes/service/superhero/SuperheroServiceTest.java
+++ src/test/java/com/davidperezpardo/superheroes/service/superhero/SuperheroServiceTest.java
@@ -6,8 +6,13 @@ package com.davidperezpardo.superheroes.service.superhero;
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -31,6 +36,9 @@ public class SuperheroServiceTest {
 
 	@Autowired
 	private SuperheroService superheroService;
+	
+	@MockBean
+	private EntityManager entityManager;
 	
 	@MockBean
 	private SuperheroRepositoryDao superheroRepository;
@@ -73,5 +81,138 @@ public class SuperheroServiceTest {
 		});
 		
 		Mockito.verify(superheroRepository, Mockito.times(1)).findByNameContainsIgnoreCaseAndDeletedAtIsNull(Mockito.anyString());
+	}
+	
+	@Test
+	void findByIdOk() throws ServiceException {
+		
+		Superhero superhero = new Superhero();
+		superhero.setId(12);
+		superhero.setName("Lobezno");
+		superhero.setCreatedAt(Date.from(Instant.now()));
+		superhero.setUpdatedAt(Date.from(Instant.now()));
+		Optional<Superhero> superheroOptional = Optional.of(superhero);
+		
+		Mockito.when(superheroRepository.findByIdAndDeletedAtIsNull(Mockito.anyInt()))
+			.thenReturn(superheroOptional);
+		
+		var result = superheroService.findById(1);
+		
+		assertEquals(result.getClass(), SuperheroDto.class);
+		
+		Mockito.verify(superheroRepository, Mockito.times(1)).findByIdAndDeletedAtIsNull(Mockito.anyInt());
+	}
+	
+	@Test
+	void findByIdKoException() throws ServiceException {
+		
+		Mockito.when(superheroRepository.findByIdAndDeletedAtIsNull(Mockito.anyInt()))
+			.thenReturn(Optional.empty());
+
+		assertThrows(ServiceException.class, () -> {
+			superheroService.findById(1);
+		});
+		
+		Mockito.verify(superheroRepository, Mockito.times(1)).findByIdAndDeletedAtIsNull(Mockito.anyInt());
+	}
+	
+	@Test
+	void deleteOk() throws ServiceException {
+		
+		Superhero superhero = new Superhero();
+		superhero.setId(12);
+		superhero.setName("Lobezno");
+		superhero.setCreatedAt(Date.from(Instant.now()));
+		superhero.setUpdatedAt(Date.from(Instant.now()));
+		Optional<Superhero> superheroOptional = Optional.of(superhero);
+		
+		Mockito.when(superheroRepository.findByIdAndDeletedAtIsNull(Mockito.anyInt()))
+			.thenReturn(superheroOptional);
+		
+		Mockito.doNothing().when(superheroRepository).softDeleteById(Mockito.anyInt());
+		
+		superheroService.delete(1);
+		
+		Mockito.verify(superheroRepository, Mockito.times(1)).findByIdAndDeletedAtIsNull(Mockito.anyInt());
+		Mockito.verify(superheroRepository, Mockito.times(1)).softDeleteById(Mockito.anyInt());
+	}
+	
+	@Test
+	void deleteKoException() throws ServiceException {
+			
+		Mockito.when(superheroRepository.findByIdAndDeletedAtIsNull(Mockito.anyInt()))
+			.thenReturn(Optional.empty());
+				
+		assertThrows(ServiceException.class, () -> {
+			superheroService.delete(1);
+		});
+		
+		Mockito.verify(superheroRepository, Mockito.times(1)).findByIdAndDeletedAtIsNull(Mockito.anyInt());
+	}
+	
+	@Test
+	void updateOk() throws ServiceException {
+		
+		Superhero superhero = new Superhero();
+		superhero.setName("Venom");
+		superhero.setCreatedAt(Date.from(Instant.now()));
+		superhero.setUpdatedAt(Date.from(Instant.now()));
+		Optional<Superhero> superheroOptional = Optional.of(superhero);
+		
+		SuperheroDto superheroDto = new SuperheroDto();
+		superheroDto.setNombre("Venom");
+		
+		Mockito.when(superheroRepository.findByIdAndDeletedAtIsNull(Mockito.anyInt()))
+			.thenReturn(superheroOptional);
+		
+		Mockito.when(superheroRepository.saveAndFlush(Mockito.any(Superhero.class)))
+			.thenReturn(superhero);
+		
+		var result = superheroService.saveOrUpdate(superheroDto, 1);
+		
+		assertEquals(result.getClass(), SuperheroDto.class);
+		
+		Mockito.verify(superheroRepository, Mockito.times(1)).findByIdAndDeletedAtIsNull(Mockito.anyInt());
+		Mockito.verify(superheroRepository, Mockito.times(1)).saveAndFlush(Mockito.any(Superhero.class));
+	}
+	
+	@Test
+	void saveOk() throws ServiceException {
+		
+		Superhero superhero = new Superhero();
+		superhero.setName("Venom");
+		superhero.setCreatedAt(Date.from(Instant.now()));
+		Optional<Superhero> superheroOptional = Optional.of(superhero);
+		
+		SuperheroDto superheroDto = new SuperheroDto();
+		superheroDto.setNombre("Venom");
+		
+		Mockito.when(superheroRepository.findByIdAndDeletedAtIsNull(Mockito.anyInt()))
+			.thenReturn(superheroOptional);
+		
+		Mockito.when(superheroRepository.saveAndFlush(Mockito.any(Superhero.class)))
+			.thenReturn(superhero);
+		
+		var result = superheroService.saveOrUpdate(superheroDto, null);
+		
+		assertEquals(result.getClass(), SuperheroDto.class);
+		
+		Mockito.verify(superheroRepository, Mockito.times(1)).saveAndFlush(Mockito.any(Superhero.class));
+	}
+	
+	@Test
+	void saveOrCreateKoException() throws ServiceException {
+				
+		SuperheroDto superheroDto = new SuperheroDto();
+		superheroDto.setNombre("Venom");
+		
+		Mockito.when(superheroRepository.findByIdAndDeletedAtIsNull(Mockito.anyInt()))
+			.thenReturn(Optional.empty());
+		
+		assertThrows(ServiceException.class, () ->{
+			superheroService.saveOrUpdate(superheroDto, 2);
+		});
+				
+		Mockito.verify(superheroRepository, Mockito.times(1)).findByIdAndDeletedAtIsNull(Mockito.anyInt());
 	}
 }
